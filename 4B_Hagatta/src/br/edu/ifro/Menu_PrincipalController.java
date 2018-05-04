0682fe3f2e65dc53f59531fb1a4e1349399d6d4b
04226528220 <04226528220@JIPA-LABIN05-01.ifro.local>
2018-05-04 15:19:50 -0400

    Criação Calculadora

--- 4B_Hagatta/src/br/edu/ifro/Menu_PrincipalController.java
+++ 4B_Hagatta/src/br/edu/ifro/Menu_PrincipalController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,17 +21,23 @@ import javafx.scene.control.Label;
 public class Menu_PrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+  private TextField txtNumero1, txtNumero2, txtResultado;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+   private Button btnSoma;
+    
+    @FXML
+    private void Soma (ActionEvent event) {
+   Double n1= Double.parseDouble(txtNumero1.getText());
+   Double n2= Double.parseDouble(txtNumero2.getText());
+   Double R= n1+n2;
+   
+   txtResultado.setText(R.toString());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+  
     }    
     
 }
